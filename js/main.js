0c40416eb2786c8e6632e1b23ad057185b609480
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2020-01-31 05:42:38 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 6.440613, lng: 3.501246 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+
+// Smooth Scrolling
+$('#navbar a, .btn #myBtn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top-100
+      },
+      80
+    );
+  }
+});
+
+// Read More Button
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("moretext");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
